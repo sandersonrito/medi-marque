d3332e216ac43c86c5e3f8851bf39d97fd744d8d
abel_barros <abel_15neto@hotmail.com>
2024-10-26 14:23:36 -0300

    Adicionando o activity_home.xml, acrescentando a cor alizarin.xml e o baseline_arrow.

--- app/src/main/java/android/moveis/dispositivo/medimarque/view/Home.kt
+++ app/src/main/java/android/moveis/dispositivo/medimarque/view/Home.kt
@@ -1,5 +1,6 @@
 package android.moveis.dispositivo.medimarque.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,20 +14,15 @@ class Home : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
         setContentView(binding.root)
 
         val nome = intent.extras?.getString("nome")
 
         binding.txtNomeUsuario.text = "Bem-vindo(a),$nome"
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
