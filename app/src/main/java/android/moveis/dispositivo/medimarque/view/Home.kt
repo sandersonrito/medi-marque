9683eb6f541328c266e2fffeea4e407018697f86
abel_barros <abel_15neto@hotmail.com>
2024-10-26 13:53:26 -0300

    Adicionando o button no activity_home.xml, acrescentando a cor alizarin.xml e o baseline_arrow.

--- app/src/main/java/android/moveis/dispositivo/medimarque/view/Home.kt
+++ app/src/main/java/android/moveis/dispositivo/medimarque/view/Home.kt
@@ -7,12 +7,21 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.moveis.dispositivo.medimarque.R
 import android.moveis.dispositivo.medimarque.R.*
+import android.moveis.dispositivo.medimarque.databinding.ActivityHomeBinding
 
 class Home : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(layout.activity_home)
+        setContentView(binding.root)
+
+        val nome = intent.extras?.getString("nome")
+
+        binding.txtNomeUsuario.text = "Bem-vindo(a),$nome"
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
