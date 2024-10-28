847a074516e48ecda1e8e8378accafc16b81092f
abel_barros <abel_15neto@hotmail.com>
2024-10-28 13:20:31 -0300

    Criando a especialidade Cardiologista e implementando algumas caracteristicas.

--- app/src/main/java/android/moveis/dispositivo/medimarque/adapter/EspecialidadesAdapter.kt
+++ app/src/main/java/android/moveis/dispositivo/medimarque/adapter/EspecialidadesAdapter.kt
@@ -1,4 +1,30 @@
 package android.moveis.dispositivo.medimarque.adapter
 
-class EspecialidadesAdapter {
+import android.content.Context
+import android.moveis.dispositivo.medimarque.databinding.EspecialidadesItemBinding
+import android.moveis.dispositivo.medimarque.model.Especialidades
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class EspecialidadesAdapter(private val context: Context, private val listaEspecialidades: MutableList<Especialidades>):
+    RecyclerView.Adapter<EspecialidadesAdapter.EspecialidadesViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EspecialidadesViewHolder {
+        val itemLista = EspecialidadesItemBinding.inflate(LayoutInflater.from(context),parent,false)
+        return EspecialidadesViewHolder(itemLista)
+    }
+
+    override fun getItemCount() = listaEspecialidades.size
+
+    override fun onBindViewHolder(holder: EspecialidadesViewHolder, position: Int) {
+        holder.imgEspecialidade.setImageResource(listaEspecialidades[position].img!!)
+        holder.txtEspecialidade.text = listaEspecialidades[position].nome
+    }
+
+    inner class EspecialidadesViewHolder(binding: EspecialidadesItemBinding): RecyclerView.ViewHolder(binding.root){
+        val imgEspecialidade = binding.imgEspecialidade
+        val txtEspecialidade = binding.txtEspecialidade
+    }
 }
--- app/src/main/java/android/moveis/dispositivo/medimarque/view/Home.kt
+++ app/src/main/java/android/moveis/dispositivo/medimarque/view/Home.kt
@@ -7,11 +7,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.moveis.dispositivo.medimarque.R.*
+import android.moveis.dispositivo.medimarque.adapter.EspecialidadesAdapter
 import android.moveis.dispositivo.medimarque.databinding.ActivityHomeBinding
+import android.moveis.dispositivo.medimarque.model.Especialidades
+import androidx.recyclerview.widget.GridLayoutManager
 
 class Home : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var especialidadesAdapter: EspecialidadesAdapter
+    private val listaEspecialidades: MutableList<Especialidades> = mutableListOf()
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +28,15 @@ class Home : AppCompatActivity() {
         val nome = intent.extras?.getString("nome")
 
         binding.txtNomeUsuario.text = "Bem-vindo(a),$nome"
-
+        val recyclerViewEspecialidades = binding.recyclerViewEspecialidades
+        recyclerViewEspecialidades.layoutManager = GridLayoutManager(this, 2)
+        especialidadesAdapter = especialidadesAdapter(this,listaEspecialidades)
+        recyclerViewEspecialidades.setHasFixedSize(true)
+        recyclerViewEspecialidades.adapter = especialidadesAdapter
     }
 
+    private fun getEspecialidades(){
+
+        val especialidade1 = Especialidades(R.drawable.img1, nome = "Cardiologista")
+    }
 }
